745ca65b676ee25e63d5c602c9cd0e71a33a7531
Yash <yash@khemani.com>
2020-04-24 05:50:14 +0000

    bit of cleanup

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "aws_region" {
+variable aws_region {
   type        = string
   description = "AWS Region in which to deploy our hashistack instance."
   default     = "us-west-2"
@@ -9,12 +9,12 @@ variable owner {
   description = "Label to identify owner, will be used for tagging resources that are provisioned."
 }
 
-variable "ssh_key_name" {
+variable ssh_key_name {
   type        = string
   description = "Label for SSH key."
 }
 
-variable "ssh_public_key" {
+variable ssh_public_key {
   type        = string
   description = "SSH public key."
 }
